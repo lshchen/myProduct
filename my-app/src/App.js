96b4aa73d863345170fe8a1d9abba025881745d8
lsc <lishaochen_vip@163.com>
2020-04-30 11:20:06 +0800

    feat: redux-saga使用

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,18 +11,6 @@ function App(props) {
   return (
     <div className="App">
       {/*<header className="App-header">*/}
-      {/*  /!*<img src={logo} className="App-logo" alt="logo" />*!/*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
       {/*  <div>{props.num}</div>*/}
       {/*  <button onClick={props.setNum}>新增</button>*/}
       {/*</header>*/}
--- my-app/src/store.js
+++ my-app/src/store.js
@@ -6,14 +6,14 @@ import scrollViewReducer from './reducers/scrollViewReducer'
 import orderReducer from "./reducers/orderReducer";
 import thunk from 'redux-thunk';
 import { routerMiddleware, connectRouter} from 'connected-react-router';
-import createSagaMiddleware from "redux-saga"
-import rootSaga from './sagas/saga';
+// import createSagaMiddleware from "redux-saga"
+// import rootSaga from './sagas/saga';
 const createHistory = require('history').createBrowserHistory
 const history = createHistory();
 const historyMiddle = routerMiddleware(history)
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
-const sagaMiddleware = createSagaMiddleware()
-const ENHANCERS = composeEnhancers(applyMiddleware(thunk,historyMiddle,sagaMiddleware));
+// const sagaMiddleware = createSagaMiddleware()
+const ENHANCERS = composeEnhancers(applyMiddleware(thunk,historyMiddle));
 const reducer = combineReducers({
     categoryReducer,
     tabReducer,
@@ -27,7 +27,7 @@ const store = createStore(
     ENHANCERS,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-sagaMiddleware.run(rootSaga)
+// sagaMiddleware.run(rootSaga)
 export {
     store,
     history
